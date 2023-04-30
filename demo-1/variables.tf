184309341035526922afb2fa107f517320e047da
Amit Barak <info.amitbarak@gmail.com>
2023-04-30 07:44:37 +0530

    Added First file to test

---
+++ demo-1/variables.tf
@@ -0,0 +1,15 @@
+variable "AWS_ACCESS_KEY" {
+}
+variable "AWS_SECRET_KEY" {
+}
+
+variable "REGION" {
+	default = "ap-south-1"
+}
+variable "AMIS" {
+	type = map(string)
+	default = {
+		ap-south-1 = "ami-02eb7a4783e7e9317"
+	}
+]
+
